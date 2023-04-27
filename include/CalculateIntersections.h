b76d4ed2345d45ced9a2c96f0952a034ece38316
Ilya Kuznetsov <9302907766@mail.ru>
2023-04-27 18:06:57 +0300

    First working version with comments in .h file (before tests)

--- include/CalculateIntersections.h
+++ include/CalculateIntersections.h
@@ -10,19 +10,26 @@ using namespace Geometry;
 
 namespace Math{
 
+    //Determinant of the 2-dimensional matrix
     coord_t det(coord_t a1, coord_t a2 ,coord_t b1, coord_t b2);
 
+    //Pushing point into vector only if it is a new point of intersection
     void push_back_unique(std::vector<Point>& list, Point& point);
 
+    //Checking that point is locating into polygon (inside of on the border)
     bool isPointInside(Point& point, Polygon& figure);
 
+    //Adding into vector all vertex of "internal" polygon which is locating into "external polygon"
     void addInsideVertex(std::vector<Point>& list, Polygon& internal, Polygon& external);
 
+    //Called when we already know all points of intersection, we sort their in right to output order
     std::vector<Point> sortPoints(std::vector<Point> list);
 
+    //Important case - it is when 2 lines not parallel and sections have intersection. If case is important, we get coords of intersection point, else we get (0,0) point
     Point findLinesInter(Point& firstStart, Point& firstEnd,\
         Point& secondStart,Point& secondEnd, bool& isImportantCase);
 
+    //main function of math module, find intersection area
     Intersection findTriangleInter(Polygon& first, Polygon& second);
 
 }
