650e7a30b1c7a8b5c07b92c629281f850c802661
Ilya Kuznetsov <9302907766@mail.ru>
2023-04-27 04:42:30 +0300

    Working finding the intersection of two sections and working checking the location of point (in polygon ot not). Algorithm of creating polygon not written yet, but it's working in the easiest cases

--- src/CalculateIntersections.cpp
+++ src/CalculateIntersections.cpp
@@ -12,7 +12,7 @@ namespace Math {
     }
 
     void push_back_unique(std::vector<Point> &list, Point &point) {
-        if (std::find(list.begin(), list.end(), point) != list.end()) {
+        if (std::find(list.begin(), list.end(), point) == list.end()) {
             list.push_back(point);
         }
     };
@@ -27,13 +27,42 @@ namespace Math {
         }
     }
 
-    bool isPointInside(Point &point, Polygon &figure) {
-        bool left, right = false;
-        coord_t pointInEdge;
+    bool isPointInside(Point& point, Polygon &figure) {
+        bool left = false, right = false;
+        coord_t k;
+        coord_t X0, dY, interX;
         for (size_t edge = 0; edge < figure.size(); edge++) {
+            if (figure[edge].getY() == point.getY() && figure[(edge + 1) % figure.size()].getY() == point.getY()){
+                return false;
+            }
             if (std::min(figure[edge].getY(), figure[(edge + 1) % figure.size()].getY()) <= point.getY() && \
             std::max(figure[edge].getY(), figure[(edge + 1) % figure.size()].getY()) >= point.getY()) {
-
+                if (figure[edge].getX() - figure[(edge + 1) % figure.size()].getX()==0){
+                    if (figure[edge].getX()<point.getX()){
+                        left=true;
+                    }
+                    else if (figure[edge].getX()>point.getX()){
+                        right=true;
+                    }
+                }
+                else{
+                    k=(figure[(edge + 1) % figure.size()].getX()-figure[edge].getX())/(figure[(edge + 1) % figure.size()].getY()-figure[edge].getY());
+                    if (figure[edge].getY()==std::min(figure[edge].getY(), figure[(edge + 1) % figure.size()].getY())){
+                        X0 = figure[edge].getX();
+                        dY = point.getY() - figure[edge].getY();
+                    }
+                    else{
+                        X0=figure[(edge + 1) % figure.size()].getX();
+                        dY = point.getY() - figure[(edge + 1) % figure.size()].getY();
+                    }
+                    interX=X0+k*dY;
+                    if (interX < point.getX()){
+                        left = true;
+                    }
+                    else if (interX > point.getX()){
+                        right = true;
+                    }
+                }
             }
             if (left && right) {
                 return true;
@@ -55,7 +84,7 @@ namespace Math {
         coord_t determinant = det(tFirstX, tSecondX, tFirstY, tSecondY);
         if (determinant == 0) {
 
-            if (b1 == b2) {
+            if (tFirstX/tFirstY == b1/b2) {
 
                 if (std::max(firstStart.getX(), firstEnd.getX()) < std::min(secondStart.getX(), secondEnd.getX()) || \
                  std::min(firstStart.getX(), firstEnd.getX()) > std::max(secondStart.getX(), secondEnd.getX())) {
@@ -71,12 +100,24 @@ namespace Math {
                 std::min(firstStart.getX(), firstEnd.getX()) <= std::min(secondStart.getX(), secondEnd.getX())) {
                     state = SectionsState::SecondFullInFirst;
                     return Point(0, 0);
-                } else if (std::min(secondStart.getX(), secondEnd.getX()) > firstStart.getX() &&
+                } else if (std::min(secondStart.getX(), secondEnd.getX()) < firstStart.getX() &&
                            firstStart.getX() < std::max(secondStart.getX(), secondEnd.getX())) {
                     state = SectionsState::StartInEndOut;
-                } else if (std::min(secondStart.getX(), secondEnd.getX()) > firstEnd.getX() &&
+                    if (std::min(firstStart.getX(), firstEnd.getX())<secondStart.getX() &&  std::max(firstStart.getX(), firstEnd.getX())>secondStart.getX()){
+                        return secondStart;
+                    }
+                    else {
+                        return secondEnd;
+                    }
+                } else if (std::min(secondStart.getX(), secondEnd.getX()) < firstEnd.getX() &&
                            firstEnd.getX() < std::max(secondStart.getX(), secondEnd.getX())) {
                     state = SectionsState::StartOutEndIn;
+                    if (std::min(firstStart.getX(), firstEnd.getX())<secondStart.getX() &&  std::max(firstStart.getX(), firstEnd.getX())>secondStart.getX()){
+                        return secondStart;
+                    }
+                    else {
+                        return secondEnd;
+                    }
                 } else if (firstStart.getX() == secondStart.getX() || firstStart.getX() == secondStart.getY()) {
                     state = SectionsState::Dot;
                     return firstStart;
@@ -90,11 +131,13 @@ namespace Math {
                 return Point(0, 0);
             }
 
-        } else {
-            interX = tSecondY * b1 - tFirstY * b2;
-            interY = tFirstX * b2 - tSecondX * b1;
-            if (interX >= 0 && interY <= 1 && interY >= 0 && interY <= 1) {
-                return Point(tFirstX * interX, tFirstX * interY);
+        }
+        else {
+            interX = (tSecondY * b1 - tSecondX * b2)/determinant;
+            interY = (tFirstX * b2 - tFirstY * b1)/determinant;
+            if (interX >= 0 && interX <= 1 && interY >= 0 && interY <= 1) {
+                state = SectionsState::Dot;
+                return Point(tFirstX * interX+firstStart.getX(), tFirstY * interX+firstStart.getY());
             } else {
                 state = SectionsState::NoIntersection;
                 return Point(0, 0);
@@ -102,11 +145,27 @@ namespace Math {
         }
     }
 
-    Polygon findTriangleInter(Polygon &first, Polygon &second) {
+    Intersection findTriangleInter(Polygon &first, Polygon &second) {
         std::vector<Point> listOfInterPoints;
+        std::vector<Point> insidePointsOfDinamic;
+        for (size_t vertex = 0; vertex <= 2; vertex++){
+            if (isPointInside(second[vertex], first)){
+                insidePointsOfDinamic.push_back(second[vertex]);
+            }
+        }
+        if (insidePointsOfDinamic.size()==3){
+            return Intersection{States::IntersectionState::Nested, Polygon(insidePointsOfDinamic)};
+        }
+        if (insidePointsOfDinamic.size()==2){
+
+        }
         Point newPoint;
         SectionsState state{SectionsState::NoIntersection};
+
         for (size_t firstPoint = 0; firstPoint <= 2; firstPoint++) {
+            if (isPointInside(first[firstPoint],second)){
+                push_back_unique(listOfInterPoints,first[firstPoint]);
+            }
             std::vector<Point> intermediateList;
             for (size_t secondPoint = 0; secondPoint <= 2; secondPoint++) {
                 newPoint = findLinesInter(first[firstPoint], first[(firstPoint + 1) % 3], second[secondPoint],
@@ -139,17 +198,38 @@ namespace Math {
                 }
                 state = SectionsState::NoIntersection;
             }
-            if (intermediateList.size() == 1 && !(listOfInterPoints.empty()) &&
-                first[firstPoint] != listOfInterPoints[listOfInterPoints.size()])
+            if (intermediateList.size() == 2)
                 smartSwap(intermediateList, first[firstPoint]);
             for (size_t elem = 0; elem < intermediateList.size(); elem++) {
                 push_back_unique(listOfInterPoints, intermediateList[elem]);
             }
         }
-        if (listOfInterPoints.size() == 0) {
+        if (insidePointsOfDinamic.size()==1){
 
         }
-        return Polygon(listOfInterPoints);
+        if (listOfInterPoints.size()==2) {
+            for (size_t pointNumber=0;pointNumber<3; pointNumber++) {
+                if (!isPointInside(first[pointNumber], second)) {
+                    return Intersection{States::IntersectionState::Polygon, Polygon(listOfInterPoints)};
+                }
+            }
+            bool match;
+            for (size_t pointNumber=0;pointNumber<3; pointNumber++) {
+                match=false;
+                for (size_t vertexOfSecond=0;vertexOfSecond<3; vertexOfSecond++){
+                    if (second[vertexOfSecond]==first[pointNumber]){
+                        match=true;
+                        continue;
+                    }
+                }
+                if (!match){
+                    return Intersection{States::IntersectionState::Polygon,Polygon(listOfInterPoints)};
+                }
+            }
+            return Intersection{States::IntersectionState::Matched,Polygon(listOfInterPoints)};
+        }
+        std::cout<<listOfInterPoints.size();
+        return Intersection{States::IntersectionState::Polygon,Polygon(listOfInterPoints)};
     }
 }
 
