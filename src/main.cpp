acf96c0467d6dee900ae35759559e3deb486a8c3
GrafVonTee <grafvontee@yandex.ru>
2023-05-03 19:42:32 +0300

    Added functional helping pass unit-test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "GeometryUtils.h"
 #include "CalculateIntersections.h"
 #include "Parsing.h"
@@ -5,5 +6,9 @@
 #include "StatesUtils.h"
 
 int main() {
+    std::istringstream inpStream("(1,1)\n(2,2)\n(1,3)\n(1,1)\n(2,2)\n(1,3)\n");
+    auto [tr1, tr2] = Interaction::getBothTriangles(inpStream);
+    Interaction::printTriangle(tr1, 1);
+    Interaction::printTriangle(tr2, 2);
     return 0;
 }
