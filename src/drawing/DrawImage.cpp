efb7cdfd00f78fbb8509a71d74dacf6e730b7cb0
Danil Eremenko <dantimsonwork@gmail.com>
2023-05-05 00:34:46 +0300

    upd

--- src/drawing/DrawImage.cpp
+++ src/drawing/DrawImage.cpp
@@ -26,35 +26,54 @@ void draw_triangles_and_intersection(const ImVec2& a1, const ImVec2& a2, const I
     // Initialize GLAD
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    // Initialize ImGui
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.DisplaySize = ImVec2(512, 512);
-    io.DisplayFramebufferScale = ImVec2(1, 1);
-    ImGui::StyleColorsDark();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-    
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
 
+    
+
     // create a new window
     ImGui::SetNextWindowSize(io.DisplaySize);
     ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::Begin("My Window", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-    ImDrawList* draw_list = ImGui::GetWindowDrawList();
 
-    // draw the first triangle
-    draw_list->AddTriangle(a1, a2, a3, IM_COL32(255, 0, 0, 255), 2.0f);
 
-    // draw the second triangle
-    draw_list->AddTriangle(b1, b2, b3, IM_COL32(0, 255, 0, 255), 2.0f);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
 
-    // draw the intersection
-    draw_list->AddPolyline(&intersection_points[0], intersection_points.size(), IM_COL32(255, 255, 0, 255), true, 2.0f);
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        ImGui::Begin("My Window", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
+
+        // draw the first triangle
+        draw_list->AddTriangle(a1, a2, a3, IM_COL32(255, 0, 0, 255), 2.0f);
+
+        // draw the second triangle
+        draw_list->AddTriangle(b1, b2, b3, IM_COL32(0, 255, 0, 255), 2.0f);
+
+        // draw the intersection
+        draw_list->AddPolyline(&intersection_points[0], intersection_points.size(), IM_COL32(255, 255, 0, 255), true, 2.0f);
     
-    // end the window
-    ImGui::End();
+        // end the window
+        ImGui::End();
+
+    }
+    
 
     // Rendering
     ImGui::Render();
