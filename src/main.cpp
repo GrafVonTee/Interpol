a5cdb997fb007a5ad9498ecd9803e60229a211d4
Alxyll <118683416+Alxyll@users.noreply.github.com>
2023-05-07 02:59:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,13 @@ int main() {
     auto [b1, b2, b3] = DrawUtils::getTupleFromPolygon(tr2);
     auto intersectionPoints = DrawUtils::getVectorFromPolygon(intersection.polygon);
 
+    double scale_x, scale_y, delta_x, delta_y, min_x, min_y;
+
+    DrawUtils::FindParameters(a1, a2, a3, b1, b2, b3, 700,
+                   scale_x, scale_y, delta_x, delta_y, min_x, min_y);
+    DrawUtils::ScaleAndTranslate(a1, a2, a3, b1, b2, b3, intersectionPoints,
+                                 scale_x, scale_y, delta_x, delta_y, min_x, min_y);
+
     Interaction::welcomeToGui();
     DrawOutput::draw_triangles_and_intersection(
             a1, a2, a3,
