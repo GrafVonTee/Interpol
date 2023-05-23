b6faeb11c63c79ee5a852feb984735a36b067267
Alxyll <118683416+Alxyll@users.noreply.github.com>
2023-05-24 01:55:46 +0300

    Update ConsoleInteraction.h

--- include/ConsoleInteraction.h
+++ include/ConsoleInteraction.h
@@ -8,23 +8,27 @@ namespace Interaction {
     using point_result_t = std::tuple<Geometry::Point, States::InputState>;
     using triangle_result_t = std::tuple<Geometry::Polygon, States::InputState>;
     using triangle_pair_t = std::tuple<Geometry::Polygon, Geometry::Polygon>;
-
-    const int skipToTriangle1(Geometry::Letters::A),
-              skipToTriangle2(Geometry::Letters::D),
-              skipToIntersection(Geometry::Letters::G);
+    using polygon_result_t = std::tuple<Geometry::Polygon, States::InputState>;
+    using polygon_pair_t = std::tuple<Geometry::Polygon, Geometry::Polygon>;
 
     void greeting(const std::string &userName);
     void goodbye(const std::string &userName);
     void welcomeToGui();
     std::string getUserName();
-    triangle_result_t getTriangle(int numberOfTriangle,
+    triangle_result_t getTriangle(const std::string &letter,
                                   std::istream& inputStream = std::cin,
                                   std::ostream& outputStream = std::cout);
-    point_result_t getPoint(Geometry::Letters letter,
+    point_result_t getPoint(const std::string &letter,
                             std::istream& inputStream = std::cin,
                             std::ostream& outputStream = std::cout);
     triangle_pair_t getBothTriangles(std::istream& inputStream = std::cin,
                                      std::ostream& outputStream = std::cout);
+    polygon_result_t getPolygon(const char letter,
+                                std::istream& inputStream = std::cin,
+                                std::ostream& outputStream = std::cout);
+    polygon_pair_t getBothPolygons(std::istream& inputStream = std::cin,
+                                   std::ostream& outputStream = std::cout);
+    void askSorting(Geometry::Polygon& polygon, char letter);
 
     void printPoint(const Geometry::Point &point);
     void printTriangle(const Geometry::Polygon &triangle);
