472a1de4c7196fe8659d206a04c0d99f163e8d06
Graf von Tee <grafvontee@yandex.ru>
2023-05-26 01:55:42 +0300

    Rebuild main for polygons

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main() {
     std::string userName = Interaction::getUserName();
     Interaction::greeting(userName);
-    std::istringstream demoPoints("(200.0, 900.0)\n(200.0, 200.0)\n(900.0, 100.0)\n(100.0, 800.0)\n(500.0, 300.0)\n(700.0, 500.0)\n");
+    std::istringstream demoPoints("3\n(200.0, 900.0)\n(200.0, 200.0)\n(900.0, 100.0)\n3\n(100.0, 800.0)\n(500.0, 300.0)\n(700.0, 500.0)\n");
     std::ostringstream empty;
 
     std::istream *input = &std::cin;
