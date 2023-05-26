6093c2c78911e4768bf0ac8bba1f0aeefe4af4ba
Graf von Tee <grafvontee@yandex.ru>
2023-05-27 00:12:02 +0300

    ...

--- src/user_interaction/ConsoleInteraction.cpp
+++ src/user_interaction/ConsoleInteraction.cpp
@@ -129,6 +129,7 @@ namespace Interaction {
         return std::make_tuple(polygon, state);
     }
 
+
     point_result_t getPoint(const std::string &letter,
                             std::istream &inputStream,
                             std::ostream& outputStream)
@@ -176,6 +177,26 @@ namespace Interaction {
         }
 
         return std::make_tuple(triangle1, triangle2);
+    }
+
+    polygon_pair_t getBothPolygons(std::istream& inputStream, std::ostream& outputStream) {
+        outputStream << "The first thing you need is to define your two polygons (1 and 2)!" << std::endl;
+        Geometry::Polygon polygon1, polygon2;
+        States::InputState state;
+        for (int i = 1; i <= 2; ++i) {
+            do {
+                outputStream << std::endl;
+                std::string letterPolygon = (i == 1) ? "A" : "B";
+                auto [tuplePolygon, tupleState] = getPolygon(letterPolygon, inputStream, outputStream);
+                if (i == 1)
+                    polygon1 = tuplePolygon;
+                else
+                    polygon2 = tuplePolygon;
+                state = tupleState;
+            } while (state != States::InputState::Correct);
+        }
+
+        return std::make_tuple(polygon1, polygon2);
     }
 
     void printPoint(const Geometry::Point &point) {
@@ -234,13 +255,13 @@ namespace Interaction {
 
     std::string getTypeNameOfPolygon(const States::PolygonState &state) {
         std::string polygonTypes[] {
-            "not a polygon",
-            "point",
-            "edge",
-            "triangle",
-            "quadrilateral",
-            "pentagon",
-            "hexagon",
+                "not a polygon",
+                "point",
+                "edge",
+                "triangle",
+                "quadrilateral",
+                "pentagon",
+                "hexagon",
         };
         return polygonTypes[(unsigned int)state];
     }
@@ -269,7 +290,4 @@ namespace Interaction {
         // Print the polygon
         printPolygon(polygon);
     }
-
-
 }
-
