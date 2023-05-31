4d7b14fe4632f05269f22ff92a4c51a450142b6f
Graf von Tee <grafvontee@yandex.ru>
2023-05-31 16:09:01 +0300

    Added output stream for tests

--- tests/ConsoleInteractionTests.cpp
+++ tests/ConsoleInteractionTests.cpp
@@ -7,14 +7,17 @@ using namespace Interaction;
 
 TEST(ConsoleInteraction, ValidInputFormat) {
     std::istringstream input("(1.0, 2.0)\n");
-    Point point = get<0>(getPoint("A", input));
+    std::ostringstream output("");
+    Point point = get<0>(getPoint("A", input, output));
     ASSERT_EQ(point, Point(1.0, 2.0));
 }
 
 TEST(ConsoleInteraction, InvalidInputFormatPoint) {
     std::istringstream input("1,2\n(3.0, 4.0)\n");
+    std::ostringstream outputStream("");
+
     testing::internal::CaptureStderr();
-    Point point = get<0>(getPoint("A", input));
+    Point point = get<0>(getPoint("A", input, outputStream));
     std::string output = testing::internal::GetCapturedStderr();
     EXPECT_EQ(output, "Incorrect input! Please, enter values in CORRECT format '(x, y)'!\n");
     ASSERT_EQ(point, Point(0.0, 0.0));
@@ -22,8 +25,10 @@ TEST(ConsoleInteraction, InvalidInputFormatPoint) {
 
 TEST(ConsoleInteraction, EmptyString) {
     std::istringstream input("\n(3.0, 4.0)\n");
+    std::ostringstream outputStream("");
+
     testing::internal::CaptureStderr();
-    Point point = get<0>(getPoint("A", input));
+    Point point = get<0>(getPoint("A", input, outputStream));
     std::string output = testing::internal::GetCapturedStderr();
     EXPECT_EQ(output, "String is empty! Please, enter values in format '(x, y)'!\n");
     ASSERT_EQ(point, Point(0.0, 0.0));
@@ -31,8 +36,10 @@ TEST(ConsoleInteraction, EmptyString) {
 
 TEST(ConsoleInteraction, ReenteredInvalidInputFormatTriangle) {
     std::istringstream input("1,2\n(3.0, 4.0)\n(1.0, 2.0)\n1,2\n(1.0, 4.0)\n");
+    std::ostringstream outputStream("");
+
     testing::internal::CaptureStderr();
-    Polygon triangle = get<0>(getTriangle("A", input));
+    Polygon triangle = get<0>(getTriangle("A", input, outputStream));
     std::string output = testing::internal::GetCapturedStderr();
     EXPECT_EQ(output,
               "Incorrect input! Please, enter values in CORRECT format '(x, y)'!\n"
@@ -45,16 +52,20 @@ TEST(ConsoleInteraction, ReenteredInvalidInputFormatTriangle) {
 
 TEST(ConsoleInteraction, SamePointsTriangle) {
     std::istringstream input("(3.0, 4.0)\n(1.0, 2.0)\n(3.0, 4.0)\n");
+    std::ostringstream outputStream("");
+
     testing::internal::CaptureStderr();
-    Polygon triangle = get<0>(getTriangle("A", input));
+    Polygon triangle = get<0>(getTriangle("A", input, outputStream));
     std::string output = testing::internal::GetCapturedStderr();
     EXPECT_EQ(output, "Points: A1 and A3 are equal!\n");
 }
 
 TEST(ConsoleInteraction, NotATriangle) {
     std::istringstream input("(0.0, 0.0)\n(0.0, 2.0)\n(0.0, 4.0)\n");
+    std::ostringstream outputStream("");
+
     testing::internal::CaptureStderr();
-    Polygon triangle = get<0>(getTriangle("A", input));
+    Polygon triangle = get<0>(getTriangle("A", input, outputStream));
     std::string output = testing::internal::GetCapturedStderr();
     EXPECT_EQ(output, "Points: A3, A1, A2 are located in one line!\n");
 }
