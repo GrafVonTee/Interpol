2a1cb89be6a7ad49fcaeda773545e68ab71f382a
Kitsu <krasilnikovkolya4654@gmail.com>
2023-06-01 18:20:49 +0300

    Add more tests

---
+++ tests/MathTests.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "CalculateIntersections.h"
+
+using namespace Math;
+
+namespace DeterminantTest {
+    using test_cases_t = std::pair<std::vector<coord_t>, coord_t>;
+    class DeterminantTestInterface : public ::testing::TestWithParam<test_cases_t> {};
+
+    TEST_P(DeterminantTestInterface, CalculateDet) {
+        auto expected = std::get<1>(GetParam());
+        auto input = std::get<0>(GetParam());
+
+        SCOPED_TRACE("Test");
+
+        coord_t ans = det(input[0], input[1],input[2], input[3]);
+
+        EXPECT_EQ(expected, ans)
+        << "Test: det(" << input[0] << ", " << input[1]
+        << ", " << input[2] << ", " << input[3]
+        << ")\n"
+        << "Expected: " << expected
+        << "\nOutput: " << ans
+        << std::endl;
+    }
+}
