060804fd0e1924c9c5f4f1c26ac0b8a42879c668
Graf von Tee <grafvontee@yandex.ru>
2023-06-02 20:38:27 +0300

    Returned namespace

--- include/StatesLibrary.h
+++ include/StatesLibrary.h
@@ -3,37 +3,45 @@
 #include <vector>
 #include "GeometryUtils.h"
 
-struct FiguresState {
-    Geometry::Polygon polygon1;
-    Geometry::Polygon polygon2;
-    Geometry::Intersection intersection;
-};
+namespace Manipulator {
+    struct FiguresState {
+        Geometry::Polygon polygon1;
+        Geometry::Polygon polygon2;
+        Geometry::Intersection intersection;
+    };
 
-class StatesLibrary {
-private:
-    std::vector<FiguresState> m_states;
+    class StatesLibrary {
+    private:
+        std::vector<FiguresState> m_states;
 
-    StatesLibrary() = default;
-public:
-    StatesLibrary(const StatesLibrary&) = delete;
-    StatesLibrary& operator=(StatesLibrary&) = delete;
+        StatesLibrary() = default;
 
-    static StatesLibrary& getInstance() {
-        static StatesLibrary instance;
-        return instance;
-    }
+    public:
+        StatesLibrary(const StatesLibrary &) = delete;
 
-    bool isEmpty() const { return m_states.empty(); }
-    size_t getSize() const { return m_states.size(); }
+        StatesLibrary &operator=(StatesLibrary &) = delete;
 
-    const FiguresState& getState(size_t stateIndex = -1) const;
-    FiguresState& getStateRef(size_t stateIndex = -1);
+        static StatesLibrary &getInstance() {
+            static StatesLibrary instance;
+            return instance;
+        }
 
-    void addState(const FiguresState& state);
-    void emplaceState(FiguresState&& state);
+        bool isEmpty() const { return m_states.empty(); }
 
-    void addState(const Geometry::Polygon&, const Geometry::Polygon&, const Geometry::Intersection&);
-    void emplaceState(Geometry::Polygon&, Geometry::Polygon&, Geometry::Intersection&);
+        size_t getSize() const { return m_states.size(); }
 
-    void popState();
-};
+        const FiguresState &getState(size_t stateIndex = -1) const;
+
+        FiguresState &getStateRef(size_t stateIndex = -1);
+
+        void addState(const FiguresState &state);
+
+        void emplaceState(FiguresState &&state);
+
+        void addState(const Geometry::Polygon &, const Geometry::Polygon &, const Geometry::Intersection &);
+
+        void emplaceState(Geometry::Polygon &, Geometry::Polygon &, Geometry::Intersection &);
+
+        void popState();
+    };
+}
--- src/utils/StatesLibrary.cpp
+++ src/utils/StatesLibrary.cpp
@@ -1,69 +1,69 @@
 #include "StatesLibrary.h"
 
-const FiguresState&
-StatesLibrary::getState(size_t stateIndex) const {
-    if (isEmpty())
-        throw std::range_error("Library is empty!");
+namespace Manipulator {
+    const FiguresState &
+    StatesLibrary::getState(size_t stateIndex) const {
+        if (isEmpty())
+            throw std::range_error("Library is empty!");
 
-    if (stateIndex == -1)
-        return m_states.back();
+        if (stateIndex == -1)
+            return m_states.back();
 
-    try {
-        return m_states[stateIndex];
-    } catch (const std::range_error& err) {
-        throw std::range_error("Invalid index of state!");
+        try {
+            return m_states[stateIndex];
+        } catch (const std::range_error &err) {
+            throw std::range_error("Invalid index of state!");
+        }
     }
-}
 
-FiguresState&
-StatesLibrary::getStateRef(size_t stateIndex) {
-    if (isEmpty())
-        throw std::range_error("Library is empty!");
+    FiguresState &
+    StatesLibrary::getStateRef(size_t stateIndex) {
+        if (isEmpty())
+            throw std::range_error("Library is empty!");
 
-    if (stateIndex == -1)
-        return m_states.back();
+        if (stateIndex == -1)
+            return m_states.back();
 
-    try {
-        return m_states[stateIndex];
-    } catch (const std::range_error& err) {
-        throw std::range_error("Invalid index of state!");
+        try {
+            return m_states[stateIndex];
+        } catch (const std::range_error &err) {
+            throw std::range_error("Invalid index of state!");
+        }
     }
-}
 
-void
-StatesLibrary::addState(const FiguresState& state) {
-    m_states.push_back(state);
-}
+    void
+    StatesLibrary::addState(const FiguresState &state) {
+        m_states.push_back(state);
+    }
 
-void
-StatesLibrary::emplaceState(FiguresState&& state) {
-    m_states.emplace_back(state);
-}
+    void
+    StatesLibrary::emplaceState(FiguresState &&state) {
+        m_states.emplace_back(state);
+    }
 
-void
-StatesLibrary::addState(const Geometry::Polygon& poly1,
-              const Geometry::Polygon& poly2,
-              const Geometry::Intersection& inter)
-{
-    m_states.push_back(FiguresState{poly1, poly2, inter});
-}
+    void
+    StatesLibrary::addState(const Geometry::Polygon &poly1,
+                            const Geometry::Polygon &poly2,
+                            const Geometry::Intersection &inter) {
+        m_states.push_back(FiguresState{poly1, poly2, inter});
+    }
 
-void
-StatesLibrary::emplaceState(Geometry::Polygon& poly1,
-                  Geometry::Polygon& poly2,
-                  Geometry::Intersection& inter)
-{
-    m_states.emplace_back(FiguresState{
-        std::move(poly1),
-        std::move(poly2),
-        std::move(inter),
-    });
-}
+    void
+    StatesLibrary::emplaceState(Geometry::Polygon &poly1,
+                                Geometry::Polygon &poly2,
+                                Geometry::Intersection &inter) {
+        m_states.emplace_back(FiguresState{
+                std::move(poly1),
+                std::move(poly2),
+                std::move(inter),
+        });
+    }
 
-void
-StatesLibrary::popState() {
-    if (isEmpty())
-        throw std::underflow_error("Library is empty!");
+    void
+    StatesLibrary::popState() {
+        if (isEmpty())
+            throw std::underflow_error("Library is empty!");
 
-    m_states.pop_back();
+        m_states.pop_back();
+    }
 }
