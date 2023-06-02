8a984757d26883b01aa27b4ff52c084fbe0cea2d
Graf von Tee <grafvontee@yandex.ru>
2023-06-02 20:37:39 +0300

    Removed namespace, fixed some bugs

---
+++ include/StatesLibrary.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#include "GeometryUtils.h"
+
+struct FiguresState {
+    Geometry::Polygon polygon1;
+    Geometry::Polygon polygon2;
+    Geometry::Intersection intersection;
+};
+
+class StatesLibrary {
+private:
+    std::vector<FiguresState> m_states;
+
+    StatesLibrary() = default;
+public:
+    StatesLibrary(const StatesLibrary&) = delete;
+    StatesLibrary& operator=(StatesLibrary&) = delete;
+
+    static StatesLibrary& getInstance() {
+        static StatesLibrary instance;
+        return instance;
+    }
+
+    bool isEmpty() const { return m_states.empty(); }
+    size_t getSize() const { return m_states.size(); }
+
+    const FiguresState& getState(size_t stateIndex = -1) const;
+    FiguresState& getStateRef(size_t stateIndex = -1);
+
+    void addState(const FiguresState& state);
+    void emplaceState(FiguresState&& state);
+
+    void addState(const Geometry::Polygon&, const Geometry::Polygon&, const Geometry::Intersection&);
+    void emplaceState(Geometry::Polygon&, Geometry::Polygon&, Geometry::Intersection&);
+
+    void popState();
+};
